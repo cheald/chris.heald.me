714de66a5ec953739bfd24fa3ddae2aea40ad747
Chris Heald <cheald@gmail.com>
2016-11-30 17:04:28 -0700

    Update tagline

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd jekyll-uno
 rvm 2.0.0 do bundle
-rvm 2.0.0 do jekyll build -s . -d ../public --config ../_config.yml --incremental
+rvm 2.0.0 do jekyll build -s . -d ../public --config ../_config.yml 
 cd ..
 cp keybase.txt public/
 ln -s ../uploads public/uploads
